746eedfb36b5a8e85ed8964ac66df0f78593a0de
Aryan Kumar <aryankumar79922@gmail.com>
2025-02-10 17:46:28 +0530

    added .prettierrc
    adding basic company code rules

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Home from './home'
+import Header from '@/components/layouts/header.layout'
+import Footer from '@/components/layouts/footer.layout'
 
 const App = () => {
   return (
     <div>
-      <h1>App</h1>
+      <Header/>
       <Home/>
+      <Footer/>
     </div>
   )
 }
